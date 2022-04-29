209faa188f9da263fc82bbea108d0ef4b8c296bd
dylann <84851579+dylannpro123@users.noreply.github.com>
2022-04-29 01:47:19 -0400

    Update mod.lua

--- resources/mod.lua
+++ resources/mod.lua
@@ -18,21 +18,15 @@ local ModIDs = {
 2788229376, -- brad332 [ Buyer ] ID: 889616346428616754
 }
 
-local OwnerIDs = {
-2578531818, -- Dylann
-2788229376, -- brad
-}
 local success, err = pcall(function()
     for i,v in pairs(game.Players:GetChildren()) do
         if v.Character then
             if table.find(ModIDs,v.UserId) then
                 v.Character.Humanoid.DisplayName = "[⭐]"..v.DisplayName
-            elseif table.find(OwnerIDs,v.UserId) thenc
-                    v.Character.Humanoid.DisplayName = "[👑]"..v.DisplayName
             elseif not v.Character.Head:FindFirstChild("OriginalSize") then
                 v.Character.Humanoid.DisplayName = "[🤡]"..v.DisplayName
             end
         end
     end
 end)
-return ModIDs, OwnerIDs
+return ModIDs
