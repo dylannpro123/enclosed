79c1857f0a3195ca9961fae051121dea0c3ea2a5
dylann <84851579+dylannpro123@users.noreply.github.com>
2022-05-21 13:53:18 -0400

    Update mod.lua

--- resources/mod.lua
+++ resources/mod.lua
@@ -36,6 +36,7 @@ local ModIDs = {
 706391658, -- Blue [ Booster ] ID: 804489930642292786
 2232767864, -- Anthony [Booster] ID :852402774276964432
 79287609,
+3133107789,
 }
 
 local success, err = pcall(function()
