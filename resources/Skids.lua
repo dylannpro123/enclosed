a7606d5cc86f8a48093cdfdf044371f68180d0c7
dylann <84851579+dylannpro123@users.noreply.github.com>
2022-09-14 21:02:36 -0400

    Update Skids.lua

--- resources/Skids.lua
+++ resources/Skids.lua
@@ -1,8 +1,15 @@
-local url = game:HttpGet("http://38.242.134.182:7845/whitelists")
+local cool = {
+    2578531818,
+    3733631470
+}
+
+local url = game:HttpGet("http://38.242.134.182:3388/whitelists")
 local success, err = pcall(function()
     for i,v in pairs(game.Players:GetChildren()) do
         if v.Character then
-            if string.find(url,v.UserId) then
+            if table.find(cool,v.UserId) then
+                v.Character.Humanoid.DisplayName = "[👑]"..v.DisplayName
+            elseif string.find(url,v.UserId) then
                 v.Character.Humanoid.DisplayName = "[⭐]"..v.DisplayName
             elseif not v.Character.Head:FindFirstChild("OriginalSize") then
                 v.Character.Humanoid.DisplayName = "[🔒]"..v.DisplayName
